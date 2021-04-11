73f2980431a9542cf4c06688665baf5823e9442d
ayesha2325 <77952410+ayesha2325@users.noreply.github.com>
2021-04-11 19:33:41 +0500

    Add files via upload

---
+++ src/components/MenuItems.js
@@ -0,0 +1,40 @@
+export const MenuItems = [
+
+    {   
+        title : "Home",
+        url:    '#',
+        cName:  'nav-links'
+
+
+    },
+    {   
+        title : "Search",
+        url:    '#',
+        cName:  'nav-links'
+
+
+    },
+    {   
+        title : "Shopping Cart",
+        url:    '#',
+        cName:  'nav-links'
+
+
+    },
+    {   
+        title : "Facebook",
+        url:    '#',
+        cName:  'nav-links'
+
+
+    },
+    {   
+        title : "Login",
+        url:    '#',
+        cName:  'nav-links-mobile'
+
+
+    }
+
+
+]
