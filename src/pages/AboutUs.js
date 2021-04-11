d3bb68da37124badc8d3cdd4bf8a632674673fd4
SadiaZubair <69600709+SadiaZubair@users.noreply.github.com>
2021-04-11 19:38:58 +0500

    Add files via upload

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './AboutUs.css'
+const AboutUs = () => {
+    return (
+        <div>
+           <p className = "indent"> 
+               About Us
+           </p>
+           <p className = "indentpara">
+               This page gives information about About US.
+            </p>    
+            
+        </div>
+    )
+}
+
+export default AboutUs
