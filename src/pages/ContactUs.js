43cfcf864309499f915166e3a7710ada1f7804a7
SadiaZubair <69600709+SadiaZubair@users.noreply.github.com>
2021-04-11 19:39:13 +0500

    Add files via upload

---
+++ src/pages/ContactUs.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './AboutUs.css'
+const ConactUs = () => {
+    return (
+        <div>
+           <p className = "indent"> 
+               Contact Us
+           </p>
+           <p className = "indentpara">
+           Phone: (042) 35301886 
+           </p>   
+           <p className = "indentpara">
+           Email :decentstorejt@gmail.com
+            </p>    
+            
+        </div>
+    )
+}
+
+export default ConactUs
