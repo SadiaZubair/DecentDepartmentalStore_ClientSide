3cf0b6383161c3def3b0e96ff5c66b25ad750d09
shezanaveed <77901748+shezanaveed@users.noreply.github.com>
2021-04-11 19:30:42 +0500

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "./Footer.css";
+import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
+import {Link,useParams,useHistory,Redirect,Route} from 'react-router-dom';
+import styled from 'styled-components';
+import AboutUs from '../pages/AboutUs';
+
+const Styles = styled.div`
+  .navbar { background-color: #004C8C; }
+  a, .navbar-nav, .navbar-light .nav-link {
+    color: #ffffff;
+    &:hover { color: white; }
+  }
+  .navbar-brand {
+    font-size: 1.4em;
+    color: #9FFFCB;
+    &:hover { color: white; }
+  }
+  .form-center {
+    position: absolute !important;
+    left: 25%;
+    right: 25%;
+  }
+  
+`;
+function Footer() {
+  return (
+   
+    <div className="main-footer">
+       
+      <div className="container">
+     
+        <div className="row">
+          {/* Column1 */}
+          
+          {/* Column2 */}
+          <div className="col">
+          
+          <p>Address: Main Blvd, Block G4 Block G 4 Phase 2 Johar Town, Lahore, Punjab
+          Phone: (042) 35301886
+         Email:decentstorejt@gmail.com</p>
+          </div>
+          <div className="col">
+
+         <Nav.Item><Link to="/AboutUs" exact activeClassName ="active"  ><h6 className="col">About Us</h6></Link></Nav.Item>
+         
+         
+          <Nav.Item class="login"><Link to="/FAQs"><h6 className="col"> FAQs</h6></Link></Nav.Item> 
+
+          </div>
+
+          
+          <div className="col">
+         
+          <Nav.Item><Link to="/Privacypolicy"><h6 className="col">Privacy Policy</h6></Link></Nav.Item>
+          <Nav.Item ><Link to="/ContactUs"><h6 className="col">Contact Us</h6></Link></Nav.Item> 
+          </div>
+        </div>
+        <hr />
+      </div>
+      
+    </div>
+  );
+}
+
+export default Footer;
