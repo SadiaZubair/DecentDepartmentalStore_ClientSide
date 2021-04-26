83c573ce58783cc7812c4848fd75f682682821d2
SadiaZubair <69600709+SadiaZubair@users.noreply.github.com>
2021-04-26 18:32:03 +0500

    Add files via upload

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,20 +1,30 @@
 
-
 import {NavLink} from 'react-router-dom';
 import { Drawer as MUIDrawer, List,ListItem,ListItemIcon,ListItemText} from "@material-ui/core";
+import fire from "../fire.js";
+import Footer from './Footer'
+import SideCat from './HomePage/SideCat';
 const Categories = ({categories}) => {
-   
+
     return(
-       <>
+    
+      <div>
+
         <div className="category">
-       {categories.map((category, index)=>(
-           
-       <ul key={index} className="cat-item">
-       <NavLink className="nav-link" to={"/categories/"+ category}>{category}</NavLink>
-     </ul>
+       
+     <div className='grid2'>
+       {categories.map((category, index)=>(         
+       <ul key={index} className="cat-item" >
+       <NavLink className="nav-link" to={"/categories/"+ category} >{category}</NavLink>
+        </ul>
         ))}
         </div>
-       </>
+        </div>
+       
+      
+     
+   
+       </div>
     )
    
 }
